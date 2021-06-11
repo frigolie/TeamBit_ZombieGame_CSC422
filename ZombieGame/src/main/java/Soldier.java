0606ad40bfdee85c895afd866cddc5746e60bbd2
amandaperrine <72579909+amandaperrine@users.noreply.github.com>
2021-06-11 16:02:19 -0500

    Update Soldier.java

--- ZombieGame/src/main/java/Soldier.java
+++ ZombieGame/src/main/java/Soldier.java
@@ -1,3 +1,4 @@
+
 /**
  * Soldier Subclass of Character for ZombieGame project
  */
@@ -6,19 +7,18 @@
  * Team Project - ZombieGame
  * Concordia St. Paul - CSC 422
  * Team Bit
+ *
  * @author Amanda Perrine
  * @author Elise Frigoli
  * @author Jordan Duesterhoeft
  * @author Steven Wiltse
- * 
+ *
  * Created 06/08/2021
  */
+public class Soldier extends Survivor {
 
-public class Soldier extends Survivor{
-    
-    // Construtor 
-    public Soldier(String name){
-        super(name, 100, 10);
-    };
-    
+    public Soldier() {
+        super(100, 10);
+    }
+
 }
