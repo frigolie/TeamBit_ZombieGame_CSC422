25d09a7bf99d4569fcdff557f0fd04be6ac7a37d
amandaperrine <72579909+amandaperrine@users.noreply.github.com>
2021-06-11 16:03:00 -0500

    Update Zombie.java

--- ZombieGame/src/main/java/Zombie.java
+++ ZombieGame/src/main/java/Zombie.java
@@ -1,3 +1,4 @@
+
 /**
  * Subclass of Character for ZombieGame project - Superclass for Zombie type implementations
  */
@@ -6,16 +7,30 @@
  * Team Project - ZombieGame
  * Concordia St. Paul - CSC 422
  * Team Bit
+ *
  * @author Amanda Perrine
  * @author Elise Frigoli
  * @author Jordan Duesterhoeft
  * @author Steven Wiltse
- * 
+ *
  * Created 06/08/2021
  */
-public class Zombie extends Character{
+public class Zombie extends Character {
 
-    public Zombie(String name, int health, int attack) {
-        super(name, health, attack);
+    public Zombie(int health, int attack) {
+        super(health, attack);
     }
+
+    @Override
+    public void attack(Character[] characters) {
+        if (this.getHealth() > 0) {
+            for (Character character : characters) {
+                if (character instanceof Survivor) {
+                    character.setHealth(
+                            character.getHealth() - this.getAttack());
+                }
+            }
+        }
+    }
+
 }
