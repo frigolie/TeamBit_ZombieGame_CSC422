c0ceaf8c38e08cb605e2c43d0d7e1f8b14b2d1dd
amandaperrine <72579909+amandaperrine@users.noreply.github.com>
2021-06-11 16:02:36 -0500

    Update Tank.java

--- ZombieGame/src/main/java/Tank.java
+++ ZombieGame/src/main/java/Tank.java
@@ -1,23 +1,23 @@
+
 /**
  * Tank Subclass of Character for ZombieGame project
  */
-
 /**
  * Team Project - ZombieGame
  * Concordia St. Paul - CSC 422
  * Team Bit
+ *
  * @author Amanda Perrine
  * @author Elise Frigoli
  * @author Jordan Duesterhoeft
  * @author Steven Wiltse
- * 
+ *
  * Created 06/08/2021
  */
-public class Tank extends Zombie{
-    
-    // Construtor 
-    public Tank(String name){
-        super(name, 150, 20);
-    };
-    
+public class Tank extends Zombie {
+
+    public Tank() {
+        super(150, 20);
+    }
+
 }
