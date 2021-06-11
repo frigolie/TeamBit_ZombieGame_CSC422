4254ab4bdb278a0bfc540f7f9f2dfaaf75405625
amandaperrine <72579909+amandaperrine@users.noreply.github.com>
2021-06-11 16:01:55 -0500

    Update CommonInfected.java

--- ZombieGame/src/main/java/CommonInfected.java
+++ ZombieGame/src/main/java/CommonInfected.java
@@ -1,3 +1,4 @@
+
 /**
  * CommonInfected Subclass for ZombieGame project
  */
@@ -6,19 +7,17 @@
  * Team Project - ZombieGame
  * Concordia St. Paul - CSC 422
  * Team Bit
+ *
  * @author Amanda Perrine
  * @author Elise Frigoli
  * @author Jordan Duesterhoeft
  * @author Steven Wiltse
- * 
+ *
  * Created 06/08/2021
  */
+public class CommonInfected extends Zombie {
 
-public class CommonInfected extends Zombie{
-    
-    // Construtor 
-    public CommonInfected(String name){
-        super(name, 30, 5);
-    };
-    
+    public CommonInfected() {
+        super(30, 5);
+    }
 }
