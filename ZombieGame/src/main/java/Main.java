13586fd7cdf9999fa4350e468368244d8781fe2a
amandaperrine <72579909+amandaperrine@users.noreply.github.com>
2021-06-11 16:02:07 -0500

    Update Main.java

--- ZombieGame/src/main/java/Main.java
+++ ZombieGame/src/main/java/Main.java
@@ -14,50 +14,118 @@
  * Created 06/08/2021
  */
 public class Main {
+
     public static void main(String[] args) {
-        
-        // Code to call GameView Class (user interface)
-        GameView view = new GameView();
-        
-        // ****************  NOTE TEST CODE delete when not needed ***************
-        ZombieParty party = new ZombieParty(10);
-        
-        for(Character i : party){
-            System.out.println(i.getName());
+
+        int charaterNum = (int) (Math.random() * 30);
+        Character[] characters = generate(charaterNum);
+        int survivorNum = getSurvivorNum(characters);
+        int zombieNum = getZombieNum(characters);
+        System.out.printf("We have %d survivors "
+                + "trying to make it to safety\n", survivorNum);
+        System.out.printf("But there are %d zombie waiting for them.\n",
+                zombieNum);
+        fight(characters);
+
+        survivorNum = getSurvivorNum(characters);
+
+        if (survivorNum > 0) {
+            System.out.printf("It seems %d have made it to safety\n",
+                    survivorNum);
+        } else {
+            System.out.println("None of survivors made it");
         }
-        
-        Party party2 = new SurvivorParty(10);
-        
-        for(Character i : party2){
-            System.out.println(i.getName());
-            
+    }
+
+    private static int getSurvivorNum(Character[] characters) {
+        int counter = 0;
+        for (Character character : characters) {
+            if (character instanceof Survivor
+                    && character.getHealth() > 0) {
+                counter++;
+            }
         }
-        System.out.println(party.size());
-        party.remove(2);
-        
-        System.out.println(party.size());
-        
-        Tank t1 = new Tank();
-        Tank t2 = new Tank();
-        ZombieParty party3 = new ZombieParty();
-        party3.add(t1);
-        party3.add(t2);
-        
-        for(int i = 0; i < party3.size(); i++){
-            System.out.println(party3.get(i).getName());
-            
+        return counter;
+    }
+
+    private static int getZombieNum(Character[] characters) {
+        int counter = 0;
+        for (Character character : characters) {
+            if (character instanceof Zombie && character.getHealth() > 0) {
+                counter++;
+            }
         }
-        
-        Tank t3 = new Tank();
-        System.out.println(t3.getName() + "  H:" +  t3.getHealth());
-        System.out.println(t3.isDead());
-        t3.takeDamage(40);
-        System.out.println(t3.getName() + "  H:" +  t3.getHealth());
-        t3.takeDamage(400);
-        System.out.println(t3.getName() + "  H:" +  t3.getHealth());
-        System.out.println(t3.isDead());
-        // NOTE END OF TEST CODE
-        
+        return counter;
+    }
+
+    private static void fight(Character[] characters) {
+        while (runOutOfSurvivor(characters) == false
+                && runOutOfZombie(characters) == false) {
+            // survior attack first
+            for (Character character : characters) {
+                if (character instanceof Survivor) {
+                    character.attack(characters);
+                }
+            }
+
+            /// zombie attacks
+            for (Character character : characters) {
+                if (character instanceof Zombie) {
+                    character.attack(characters);
+                }
+            }
+        }
+
+    }
+
+    private static boolean runOutOfZombie(Character[] characters) {
+        for (Character character : characters) {
+            if (character instanceof Zombie) {
+                if (character.getHealth() > 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean runOutOfSurvivor(Character[] characters) {
+        for (Character character : characters) {
+            if (character instanceof Survivor) {
+                if (character.getHealth() > 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static Character[] generate(int charaterNum) {
+        // program randomly generate an array of survior and soombies
+        Character[] characters = new Character[charaterNum];
+
+        for (int i = 0; i < characters.length; i++) {
+            //Character character = characters[i];
+            int type = (int) (Math.random() * 5);
+            switch (type) {
+                case 0:
+                    characters[i] = new Child();
+                    break;
+                case 1:
+                    characters[i] = new CommonInfected();
+                    break;
+                case 2:
+                    characters[i] = new Soldier();
+                    break;
+                case 3:
+                    characters[i] = new Tank();
+                    break;
+                default:
+                    characters[i] = new Teacher();
+                    break;
+            }
+        }
+        return characters;
     }
 }
 
