051b8db2a2a950f11cc1b50f4c9813aafd63bd0b
amandaperrine <72579909+amandaperrine@users.noreply.github.com>
2021-06-12 10:13:26 -0500

    Update Zombie.java

--- ZombieGame/src/main/java/Zombie.java
+++ ZombieGame/src/main/java/Zombie.java
@@ -2,7 +2,6 @@
 /**
  * Subclass of Character for ZombieGame project - Superclass for Zombie type implementations
  */
-
 /**
  * Team Project - ZombieGame
  * Concordia St. Paul - CSC 422
@@ -17,17 +16,39 @@
  */
 public class Zombie extends Character {
 
-    public Zombie(int health, int attack) {
-        super(health, attack);
+    /**
+     * constructor
+     *
+     * @param no
+     * @param health
+     * @param attack
+     */
+    public Zombie(int no, int health, int attack) {
+        super(no, health, attack);
     }
 
+    /**
+     * zombie attacks
+     *
+     * @param characters
+     */
     @Override
     public void attack(Character[] characters) {
+        // it should be alive
         if (this.getHealth() > 0) {
             for (Character character : characters) {
-                if (character instanceof Survivor) {
+                // attack an alive survivor
+                if (character instanceof Survivor
+                        && character.getHealth() > 0) {
                     character.setHealth(
                             character.getHealth() - this.getAttack());
+                    // if the survivor is out ot the fight
+                    // show a message
+                    if (character.getHealth() <= 0) {
+                        System.out.println("\t"
+                                + this.toString()
+                                + " killed " + character.toString());
+                    }
                 }
             }
         }
