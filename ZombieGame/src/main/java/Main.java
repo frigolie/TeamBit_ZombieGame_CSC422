48eb1ebc6c7db8f0daf1263bac0dbf6dbbdfcc60
JordanD-CSP <84149503+JordanD-CSP@users.noreply.github.com>
2021-06-12 16:22:58 -0500

    Added BuildControl.java

--- ZombieGame/src/main/java/Main.java
+++ ZombieGame/src/main/java/Main.java
@@ -19,45 +19,6 @@ public class Main {
         // Code to call GameView Class (user interface)
         GameView view = new GameView();
         
-        // ****************  NOTE TEST CODE delete when not needed ***************
-        ZombieParty party = new ZombieParty(10);
-        
-        for(Character i : party){
-            System.out.println(i.getName());
-        }
-        
-        Party party2 = new SurvivorParty(10);
-        
-        for(Character i : party2){
-            System.out.println(i.getName());
-            
-        }
-        System.out.println(party.size());
-        party.remove(2);
-        
-        System.out.println(party.size());
-        
-        Tank t1 = new Tank();
-        Tank t2 = new Tank();
-        ZombieParty party3 = new ZombieParty();
-        party3.add(t1);
-        party3.add(t2);
-        
-        for(int i = 0; i < party3.size(); i++){
-            System.out.println(party3.get(i).getName());
-            
-        }
-        
-        Tank t3 = new Tank();
-        System.out.println(t3.getName() + "  H:" +  t3.getHealth());
-        System.out.println(t3.isDead());
-        t3.takeDamage(40);
-        System.out.println(t3.getName() + "  H:" +  t3.getHealth());
-        t3.takeDamage(400);
-        System.out.println(t3.getName() + "  H:" +  t3.getHealth());
-        System.out.println(t3.isDead());
-        // NOTE END OF TEST CODE
-        
     }
 }
 
