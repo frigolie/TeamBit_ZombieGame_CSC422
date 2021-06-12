3106ac049038f93aede5df800283f6d222fc933d
amandaperrine <72579909+amandaperrine@users.noreply.github.com>
2021-06-12 10:13:04 -0500

    Update Main.java

--- ZombieGame/src/main/java/Main.java
+++ ZombieGame/src/main/java/Main.java
@@ -1,42 +1,93 @@
+
 /**
  * Main Class for ZombieGame project
  */
-
 /**
  * Team Project - ZombieGame
  * Concordia St. Paul - CSC 422
  * Team Bit
+ *
  * @author Amanda Perrine
  * @author Elise Frigoli
  * @author Jordan Duesterhoeft
  * @author Steven Wiltse
- * 
+ *
  * Created 06/08/2021
  */
 public class Main {
 
+    /**
+     * main function
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 
+        // randomize number of character 
         int charaterNum = (int) (Math.random() * 30);
+
+        // get all random charaters 
         Character[] characters = generate(charaterNum);
+
+        // count each kind of character in the array
         int survivorNum = getSurvivorNum(characters);
         int zombieNum = getZombieNum(characters);
+
+        int childNum = getCharacterNum(characters, new Child());
+        int commonInfectedNum = getCharacterNum(characters,
+                new CommonInfected());
+        int soldierNum = getCharacterNum(characters, new Soldier());
+        int tankNum = getCharacterNum(characters, new Tank());
+        int teacherNum = getCharacterNum(characters, new Teacher());
+
         System.out.printf("We have %d survivors "
-                + "trying to make it to safety\n", survivorNum);
-        System.out.printf("But there are %d zombie waiting for them.\n",
-                zombieNum);
+                + "trying to make it to safety"
+                + " (%d children, %d teachers, %d soliders)\n",
+                survivorNum, childNum, teacherNum, soldierNum);
+        System.out.printf("But there are %d zombies waiting for them"
+                + "(%d common infected, %d tanks)\n",
+                zombieNum, commonInfectedNum, tankNum);
+
+        // let them fight
         fight(characters);
 
+        // count number of survivor 
         survivorNum = getSurvivorNum(characters);
 
+        // show result
         if (survivorNum > 0) {
             System.out.printf("It seems %d have made it to safety\n",
                     survivorNum);
         } else {
-            System.out.println("None of survivors made it");
+            System.out.println("None of survivor made it");
         }
     }
 
+    /**
+     * count number of a certain class in the array which are alive
+     *
+     * @param characters the array
+     * @param sample a sample object of a certain class
+     * @return number of object which is the same class to the sample object
+     */
+    private static int getCharacterNum(Character[] characters,
+            Character sample) {
+        int counter = 0;
+        for (Character character : characters) {
+            if (character.getClass() == sample.getClass()
+                    && character.getHealth() > 0) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    /**
+     * get number of alive object which is an instance of Survivor class
+     *
+     * @param characters
+     * @return number of matched object
+     */
     private static int getSurvivorNum(Character[] characters) {
         int counter = 0;
         for (Character character : characters) {
@@ -48,6 +99,12 @@ public class Main {
         return counter;
     }
 
+    /**
+     * get number of alive object which is an instance of Zombie class
+     *
+     * @param characters the array
+     * @return number of matched object
+     */
     private static int getZombieNum(Character[] characters) {
         int counter = 0;
         for (Character character : characters) {
@@ -58,6 +115,11 @@ public class Main {
         return counter;
     }
 
+    /**
+     * fighting
+     *
+     * @param characters the array of object
+     */
     private static void fight(Character[] characters) {
         while (runOutOfSurvivor(characters) == false
                 && runOutOfZombie(characters) == false) {
@@ -78,6 +140,11 @@ public class Main {
 
     }
 
+    /**
+     *
+     * @param characters
+     * @return true if all zombies are out of the fight
+     */
     private static boolean runOutOfZombie(Character[] characters) {
         for (Character character : characters) {
             if (character instanceof Zombie) {
@@ -89,6 +156,11 @@ public class Main {
         return true;
     }
 
+    /**
+     *
+     * @param characters
+     * @return true if all survivors are out of the fight
+     */
     private static boolean runOutOfSurvivor(Character[] characters) {
         for (Character character : characters) {
             if (character instanceof Survivor) {
@@ -100,6 +172,12 @@ public class Main {
         return true;
     }
 
+    /**
+     * generate an array of random objects
+     *
+     * @param charaterNum size of the array
+     * @return the array
+     */
     private static Character[] generate(int charaterNum) {
         // program randomly generate an array of survior and soombies
         Character[] characters = new Character[charaterNum];
@@ -128,4 +206,3 @@ public class Main {
         return characters;
     }
 }
-
