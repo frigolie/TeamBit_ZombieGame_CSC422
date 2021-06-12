f6dc6a37f32bde71fd4b9c9e29657fc86768785f
amandaperrine <72579909+amandaperrine@users.noreply.github.com>
2021-06-12 10:13:09 -0500

    Update Soldier.java

--- ZombieGame/src/main/java/Soldier.java
+++ ZombieGame/src/main/java/Soldier.java
@@ -2,7 +2,6 @@
 /**
  * Soldier Subclass of Character for ZombieGame project
  */
-
 /**
  * Team Project - ZombieGame
  * Concordia St. Paul - CSC 422
@@ -17,8 +16,23 @@
  */
 public class Soldier extends Survivor {
 
+    // static number 
+    private static int number = 1;
+
+    /**
+     * constructor
+     */
     public Soldier() {
-        super(100, 10);
+        super(number, 100, 10);
+        number++;
     }
 
+    /**
+     *
+     * @return name of the object
+     */
+    @Override
+    public String toString() {
+        return "Soldier " + this.getNo();
+    }
 }
