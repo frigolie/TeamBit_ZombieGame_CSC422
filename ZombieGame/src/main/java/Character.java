deea1747aacffd34357028890fb1e074e49c09d1
amandaperrine <72579909+amandaperrine@users.noreply.github.com>
2021-06-12 10:12:50 -0500

    Update Character.java

--- ZombieGame/src/main/java/Character.java
+++ ZombieGame/src/main/java/Character.java
@@ -2,7 +2,6 @@
 /**
  * Character Super Class for ZombieGame project
  */
-
 /**
  * Team Project - ZombieGame
  * Concordia St. Paul - CSC 422
@@ -17,31 +16,63 @@
  */
 public abstract class Character {
 
-    private int health;
-    private int attack;
+    private int health; // health amount 
+    private int attack; /// attack power
+    private int no; // number of order such as Tank 1, Child 1 .... 
 
-    /*
-    public Character() {
-        health = 0;
-        attack = 0;
-    }*/
-
-    public Character(int health, int attack) {
+    /**
+     * constructor
+     *
+     * @param no
+     * @param health
+     * @param attack
+     */
+    public Character(int no, int health, int attack) {
         this.health = health;
         this.attack = attack;
+        this.no = no;
     }
 
+    /**
+     * getter
+     *
+     * @return health
+     */
     public int getHealth() {
         return health;
     }
 
+    /**
+     * getter
+     *
+     * @return attack
+     */
     public int getAttack() {
         return attack;
     }
 
+    /**
+     * setter
+     *
+     * @param health new health
+     */
     public void setHealth(int health) {
         this.health = health;
     }
 
+    /**
+     * getter
+     *
+     * @return number of order
+     */
+    public int getNo() {
+        return no;
+    }
+
+    /**
+     * abstract method which will be implemented in derived class
+     *
+     * @param characters
+     */
     public abstract void attack(Character[] characters);
 }
