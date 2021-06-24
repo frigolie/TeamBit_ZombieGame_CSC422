6fe3c73a930f52150188d908736c02d183131b1d
amandaperrine <72579909+amandaperrine@users.noreply.github.com>
2021-06-24 13:27:36 -0500

    Update ZombieParty.java

--- ZombieGame/src/main/java/zombiegame/app/party/ZombieParty.java
+++ ZombieGame/src/main/java/zombiegame/app/party/ZombieParty.java
@@ -1,8 +1,11 @@
+<<<<<<< Updated upstream:ZombieGame/src/main/java/zombiegame/app/party/ZombieParty.java
 package zombiegame.app.party;
 
 import zombiegame.app.character.Tank;
 import zombiegame.app.character.Character;
 import zombiegame.app.character.CommonInfected;
+=======
+>>>>>>> Stashed changes:ZombieGame/src/main/java/ZombieParty.java
 
 /**
  * Creates of Party Object (collection) of the Survivor type
@@ -12,24 +15,31 @@ import zombiegame.app.character.CommonInfected;
  * Team Project - ZombieGame
  * Concordia St. Paul - CSC 422
  * Team Bit
+ *
  * @author Amanda Perrine
  * @author Elise Frigoli
  * @author Jordan Duesterhoeft
  * @author Steven Wiltse
- * 
+ *
  * Created 06/08/2021
  */
-public class ZombieParty extends Party{
-    
+public class ZombieParty extends Party {
+
     // Constructors
     // Default
+<<<<<<< Updated upstream:ZombieGame/src/main/java/zombiegame/app/party/ZombieParty.java
     public ZombieParty(){}
     
+=======
+    public ZombieParty() {
+    }
+
+>>>>>>> Stashed changes:ZombieGame/src/main/java/ZombieParty.java
     @Override
     public int[] getPartyInfo() {
         int numberOfCommonInfected = 0;
         int numberOfTanks = 0;
-        
+
         for (Character survivor : this) {
             if (survivor instanceof CommonInfected) {
                 numberOfCommonInfected++;
@@ -37,9 +47,9 @@ public class ZombieParty extends Party{
                 numberOfTanks++;
             }
         }
-        
+
         int[] partyInfo = {numberOfCommonInfected, numberOfTanks};
         return partyInfo;
     }
-    
+
 }
