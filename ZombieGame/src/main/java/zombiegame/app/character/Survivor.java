8fc206276dc5b024b3e80e96215d0e1826d9028e
amandaperrine <72579909+amandaperrine@users.noreply.github.com>
2021-06-24 13:26:47 -0500

    Update Survivor.java

--- ZombieGame/src/main/java/zombiegame/app/character/Survivor.java
+++ ZombieGame/src/main/java/zombiegame/app/character/Survivor.java
@@ -1,4 +1,7 @@
+<<<<<<< Updated upstream:ZombieGame/src/main/java/zombiegame/app/character/Survivor.java
 package zombiegame.app.character;
+=======
+>>>>>>> Stashed changes:ZombieGame/src/main/java/Survivor.java
 
 /**
  * Subclass of Character for ZombieGame project - Superclass for Survivor type implementations
@@ -8,18 +11,46 @@ package zombiegame.app.character;
  * Team Project - ZombieGame
  * Concordia St. Paul - CSC 422
  * Team Bit
+ *
  * @author Amanda Perrine
  * @author Elise Frigoli
  * @author Jordan Duesterhoeft
  * @author Steven Wiltse
- * 
+ *
  * Created 06/08/2021
  */
-public class Survivor extends Character{
+public class Survivor extends Character {
+
+    private Weapon weapon;
+    // there are 7 type of weapon
+    private static Weapon[] weapons = {
+        new Shotgun(), new SubmachineGun(),
+        new AssaultRifle(), new Axe(), new Crowbar(),
+        new FryingPan(), new Pistol()
+    };
 
     public Survivor(String name, int health, int attack) {
         super(name, health, attack);
+        weapon = weapons[(int) (Math.random() * weapons.length)];
     }
-    
-}
 
+    /**
+     * survivor could use weapon
+     *
+     * @return
+     */
+    @Override
+    int getAttack() {
+        return super.getAttack() + weapon.power();
+    }
+
+    /**
+     * getter
+     *
+     * @return
+     */
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+}
