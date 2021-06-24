9a10981e4a63bfcf6147a10ea351b7698dc45374
amandaperrine <72579909+amandaperrine@users.noreply.github.com>
2021-06-24 13:23:45 -0500

    Create FryingPan.java

---
+++ ZombieGame/src/main/java/FryingPan.java
@@ -0,0 +1,20 @@
+
+/**
+ * Team Project - ZombieGame
+ * Concordia St. Paul - CSC 422
+ * Team Bit
+ *
+ * @author Amanda Perrine
+ * @author Elise Frigoli
+ * @author Jordan Duesterhoeft
+ * @author Steven Wiltse
+ *
+ * Created 06/23/2021
+ */
+public class FryingPan extends Weapon {
+
+    public FryingPan() {
+        super(6, 0.73, "Frying Pan");
+    }
+
+}
