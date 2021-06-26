bf2ff0cde51f74b5c126104f24986c84c7448463
JordanD-CSP <84149503+JordanD-CSP@users.noreply.github.com>
2021-06-25 20:02:56 -0500

    Update Party.java
    - matched to existing file in /main
    - changes were out of scope for task
    - retained minor stylistic changes

--- ZombieGame/src/main/java/zombiegame/app/party/Party.java
+++ ZombieGame/src/main/java/zombiegame/app/party/Party.java
@@ -1,7 +1,4 @@
-<<<<<<< Updated upstream:ZombieGame/src/main/java/zombiegame/app/party/Party.java
 package zombiegame.app.party;
-=======
->>>>>>> Stashed changes:ZombieGame/src/main/java/Party.java
 
 /**
  * Party Class for ZombieGame project - Create collection of Character Objects
@@ -11,35 +8,31 @@ package zombiegame.app.party;
  * Team Project - ZombieGame
  * Concordia St. Paul - CSC 422
  * Team Bit
- *
+ * 
  * @author Amanda Perrine
  * @author Elise Frigoli
  * @author Jordan Duesterhoeft
  * @author Steven Wiltse
- *
+ * 
  * Created 06/08/2021
  */
-<<<<<<< Updated upstream:ZombieGame/src/main/java/zombiegame/app/party/Party.java
 
 import zombiegame.app.character.Character;
 import zombiegame.app.battle.Fatality;
 import zombiegame.app.battle.BattleInfo;
-=======
->>>>>>> Stashed changes:ZombieGame/src/main/java/Party.java
 import java.util.ArrayList; // import the ArrayList class
 
 public abstract class Party extends ArrayList<Character> {
-
+    
     // Default Constructor
-    public Party() {
-    }
-
+    public Party(){}
+    
     public void attack(Party defendingParty, BattleInfo battleInfo) {
         int attack;
         int health;
         String attackerName;
         String casualtyName;
-
+        
         for (Character attacker : this) {
             if (!attacker.isDead()) {
                 for (Character defender : defendingParty) {
@@ -51,26 +44,19 @@ public abstract class Party extends ArrayList<Character> {
                         if (defender.isDead()) {
                             attackerName = attacker.getName();
                             casualtyName = defender.getName();
-                            Fatality fatality
-                                    = new Fatality(attackerName, casualtyName);
-                            battleInfo.add(fatality);
-                            if (attacker instanceof Survivor) {
-                                Survivor survivor = (Survivor) attacker;
-                                fatality.setExtraInfor(
-                                        survivor.getWeapon().toString());
-                            }
+                            battleInfo.add(new Fatality(attackerName, casualtyName));
                         }
                     }
                 }
             }
         }
     }
-
+    
     public abstract int[] getPartyInfo();
-
+    
     public int getRemainingCharacters() {
         int remainingCharacters = 0;
-
+        
         for (Character character : this) {
             if (!character.isDead()) {
                 remainingCharacters++;
