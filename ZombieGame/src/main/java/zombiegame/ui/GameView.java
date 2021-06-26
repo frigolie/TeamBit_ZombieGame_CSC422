1f60076789e51184e185d428a3896e5cbfcf66d0
JordanD-CSP <84149503+JordanD-CSP@users.noreply.github.com>
2021-06-25 20:08:45 -0500

    Update GameView.java
    - matched to existing file in /main
    - changes were out of scope for task

--- ZombieGame/src/main/java/zombiegame/ui/GameView.java
+++ ZombieGame/src/main/java/zombiegame/ui/GameView.java
@@ -8,56 +8,45 @@ import java.util.Scanner;
 /**
  * GameView Class for ZombieGame project - Implements UI for game
  */
+
 /**
- * Team Project - ZombieGame Concordia St. Paul - CSC 422 Team Bit
- *
+ * Team Project - ZombieGame
+ * Concordia St. Paul - CSC 422
+ * Team Bit
  * @author Amanda Perrine
  * @author Elise Frigoli
  * @author Jordan Duesterhoeft
  * @author Steven Wiltse
- *
+ * 
  * Created 06/08/2021
  */
+
 /**
- * Displays info to user and turns user input into information the system can
- * use.
+ * Displays info to user and turns user input into information the system can use.
  */
 public final class GameView {
-
+    
     private final Scanner input = new Scanner(System.in);
-
-    public GameView() {
-    }
-
+    
+    public GameView() {}
+    
     /**
      * Displays the name of each attacker-casualty pair from a battle.
-     *
+     * 
      * @param battleInfo ArrayList of Fatality objects generated during battle
      */
     public void displayBattleInfo(BattleInfo battleInfo) {
         for (Fatality fatality : battleInfo) {
             String attacker = fatality.getAttacker();
             String casualty = fatality.getCasualty();
-
-            // 
-            String extraInfor = fatality.getExtraInfor();
-            if (extraInfor == null) {
-                // zombie case
-                System.out.printf("%s killed %s\n", attacker, casualty);
-            } else {
-                // survivor case
-                System.out.printf("%s killed %s with \n\t %s\n",
-                        attacker, casualty, extraInfor);
-            }
-
+            System.out.printf("%s killed %s\n", attacker, casualty);
         }
     }
-
+    
     /**
      * Displays the Party counts after they are first initialized.
-     *
-     * @param survivorPartyInfo [numberOfChildren, numberOfTeachers,
-     * numberOfSoldiers]
+     * 
+     * @param survivorPartyInfo [numberOfChildren, numberOfTeachers, numberOfSoldiers]
      * @param zombiePartyInfo [numberOfCommonInfected, numberOfTanks]
      */
     public void displayPartyInfo(int[] survivorPartyInfo, int[] zombiePartyInfo) {
@@ -65,39 +54,38 @@ public final class GameView {
         int numberOfTeachers = survivorPartyInfo[1];
         int numberOfSoldiers = survivorPartyInfo[2];
         int numberOfSurvivors = numberOfChildren + numberOfTeachers + numberOfSoldiers;
-
+        
         int numberOfCommonInfected = zombiePartyInfo[0];
         int numberOfTanks = zombiePartyInfo[1];
         int numberOfZombies = numberOfCommonInfected + numberOfTanks;
-
+        
         System.out.printf("We have %d survivors trying to make it to safety. "
-                + "(%d children, %d teachers, and %d soldiers)\n",
-                numberOfSurvivors, numberOfChildren, numberOfTeachers,
+                + "(%d children, %d teachers, and %d soldiers)\n", 
+                numberOfSurvivors, numberOfChildren, numberOfTeachers, 
                 numberOfSoldiers);
         System.out.printf("But there are %d zombies waiting for them. (%d "
-                + "common infected, %d tanks)\n", numberOfZombies,
+                + "common infected, %d tanks)\n", numberOfZombies, 
                 numberOfCommonInfected, numberOfTanks);
     }
-
+    
     /**
-     * Prints a message about how many survivors remain after a battle is
-     * completed.
-     *
+     * Prints a message about how many survivors remain after a battle is completed.
+     * 
      * @param remainingSurvivors the number of remaining survivors
      */
     public void displayRemainingSurvivors(int remainingSurvivors) {
         if (remainingSurvivors == 0) {
             System.out.println("None of the survivors made it.");
         } else {
-            System.out.printf("It seems %d have made it to safety.\n",
+            System.out.printf("It seems %d have made it to safety.\n", 
                     remainingSurvivors);
         }
     }
-
+    
     /**
-     * Asks player if they would like to play again. Loops until player enters
-     * "y" or "n" (ignore case).
-     *
+     * Asks player if they would like to play again.
+     * Loops until player enters "y" or "n" (ignore case).
+     * 
      * @return true if "y", false if "n"
      */
     public boolean playAgain() {
@@ -112,5 +100,5 @@ public final class GameView {
             }
         } while (true);
     }
-
+    
 }
