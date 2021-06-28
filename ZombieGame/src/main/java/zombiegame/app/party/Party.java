3d92d7978b417bf77d40b92849450c9785276f7f
JordanD-CSP <84149503+JordanD-CSP@users.noreply.github.com>
2021-06-28 12:05:58 -0500

    Update Party.java
    - updated Party.attack() to make use of Equippable interface
    - added generic type to class signature
    - - updated SurvivorParty.java to match
    - - updated ZombieParty.java to match
    - - this change protects the integrity of Party descendants by disallowing Characters of the wrong type of from being added to the Party

--- ZombieGame/src/main/java/zombiegame/app/party/Party.java
+++ ZombieGame/src/main/java/zombiegame/app/party/Party.java
@@ -21,30 +21,64 @@ import zombiegame.app.character.Character;
 import zombiegame.app.battle.Fatality;
 import zombiegame.app.battle.BattleInfo;
 import java.util.ArrayList; // import the ArrayList class
+import zombiegame.app.character.Equippable;
+import zombiegame.app.weapon.Weapon;
 
-public abstract class Party extends ArrayList<Character> {
+public abstract class Party<E extends Character> extends ArrayList<E> {
     
     // Default Constructor
     public Party(){}
     
-    public void attack(Party defendingParty, BattleInfo battleInfo) {
-        int attack;
-        int health;
-        String attackerName;
-        String casualtyName;
+    /**
+     * Applies each attacker's {@code attack} to each defender's {@code health} 
+     * and adds any resulting {@code Fatality} to {@code battleInfo}.
+     * Dead attackers do not attack.
+     * Attackers do not attack dead defenders.
+     * For {@code Equippable} attacker, attempts to use {@code Weapon}.
+     * If {@code Weapon} misses, no damage is applied to defender.
+     * If {@code Weapon} hits, damage is applied to defender.
+     * If {@code Weapon} results in {@code Fatality}, {@code Weapon}'s 
+     * {@code name} is stored in new {@code Fatality}.
+     * 
+     * @param defendingParty {@code Party} to be attacked
+     * @param battleInfo used to store resulting {@code Fatalities}
+     */
+    public void attack(Party<Character> defendingParty, BattleInfo battleInfo) {
+        int attack; // attacker's attack
+        int health; // defender's health
+        String attackerName; // attacker's name
+        String casualtyName; // killed defender's name
+        String weaponName; // attacker's weapon's name
+        boolean weaponUsed; // flags if a weapon is used successfully (damage > 0);
+        Weapon weapon = new Weapon(0, 0, null); // attacker's weapon
         
         for (Character attacker : this) {
             if (!attacker.isDead()) {
                 for (Character defender : defendingParty) {
                     if (!defender.isDead()) {
+                        weaponUsed = false;
                         attack = attacker.getAttack();
                         health = defender.getHealth();
                         health -= attack;
+                        if (attacker instanceof Equippable) {
+                            weapon = ((Equippable) attacker).getWeapon();
+                            int weaponDamage = weapon.useWeapon();
+                            if (weaponDamage > 0) {
+                                health -= weaponDamage;
+                                weaponUsed = true;
+                            }
+                        }
                         defender.setHealth(health);
                         if (defender.isDead()) {
                             attackerName = attacker.getName();
                             casualtyName = defender.getName();
-                            battleInfo.add(new Fatality(attackerName, casualtyName));
+                            if (weaponUsed) {
+                                weaponName = weapon.getName();
+                            } else {
+                                weaponName = null;
+                            }
+                            battleInfo.add(new Fatality(attackerName, 
+                                    casualtyName, weaponName));
                         }
                     }
                 }
@@ -52,8 +86,19 @@ public abstract class Party extends ArrayList<Character> {
         }
     }
     
+    /**
+     * Gets number of each subtype of {@code E} in this {@code Party}.
+     * Length of returned array = number of {@code E} subtypes in {@code Party}.
+     * 
+     * @return number of each subtype of {@code E} in this {@code Party}
+     */
     public abstract int[] getPartyInfo();
     
+    /**
+     * Gets number of alive {@code Character}s in this {@code Party}.
+     * 
+     * @return number of alive {@code Character}s in this {@code Party}
+     */
     public int getRemainingCharacters() {
         int remainingCharacters = 0;
         
